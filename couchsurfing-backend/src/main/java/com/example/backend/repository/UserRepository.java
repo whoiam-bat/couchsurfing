bfaa825f7ac61f6f3350bc9d674cf6f651366f7b
Oleksii Drabchak <drabchak.aleksey@gmail.com>
2023-10-25 17:39:34 +0300

    Extended User repository functionality

--- couchsurfing-backend/src/main/java/com/example/backend/repository/UserRepository.java
+++ couchsurfing-backend/src/main/java/com/example/backend/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package com.example.backend.repository;
 
 import com.example.backend.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
+
+    Page<User> findUsersByUserInfoLocation(String location, Pageable pageable);
 
 }
