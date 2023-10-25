c7b376e525ad6fdbf35c4727d1f5b8179f14f0ac
Oleksii Drabchak <drabchak.aleksey@gmail.com>
2023-10-25 11:09:30 +0300

    Minor refactoring

--- verification-api/service/verification_service.py
+++ verification-api/service/verification_service.py
@@ -31,10 +31,10 @@ def verify(request: model.verification_request.VerificationData):
     image = decode_image(image_base64)
     img_data = process_image(image)
 
-    if len(img_data) < 4:
+    if len(img_data) < 2:
         return False
 
-    mrz = retrieve_data_from_mrz(img_data[-4])
+    mrz = retrieve_data_from_mrz(img_data[-2])
 
     return str.upper(firstname) in mrz and str.upper(lastname) in mrz
 
