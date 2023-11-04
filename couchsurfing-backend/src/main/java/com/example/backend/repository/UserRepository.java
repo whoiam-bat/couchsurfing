2da8e3765c87d7a7908a5875e8525e806ba3f8f9
Oleksii Drabchak <drabchak.aleksey@gmail.com>
2023-11-04 18:36:50 +0200

    Repository changes Added JPA methods to get hosts and surfers

--- couchsurfing-backend/src/main/java/com/example/backend/repository/UserRepository.java
+++ couchsurfing-backend/src/main/java/com/example/backend/repository/UserRepository.java
@@ -3,12 +3,25 @@ package com.example.backend.repository;
 import com.example.backend.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
 
     Page<User> findUsersByUserInfoLocation(String location, Pageable pageable);
+
+    Page<User> findUsersByUserHomeIsAcceptingGuestsAndUserInfoLocation(Boolean isAcceptingGuests,
+                                                                       String location,
+                                                                       Pageable pageable);
+
+    @Query("{ trips: { $elemMatch: { location: ?0 } } }")
+    // TODO: select all surfers
+    Page<User> findUsersByIncomingTrips(String location, LocalDate date, Pageable pageable);
 
 }
