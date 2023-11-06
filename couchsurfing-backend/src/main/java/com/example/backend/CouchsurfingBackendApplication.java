2964df0907012da223cdf479a5f504639e6a284a
Oleksii Drabchak <drabchak.aleksey@gmail.com>
2023-11-06 14:09:25 +0200

    Main calss refactor

--- couchsurfing-backend/src/main/java/com/example/backend/CouchsurfingBackendApplication.java
+++ couchsurfing-backend/src/main/java/com/example/backend/CouchsurfingBackendApplication.java
@@ -10,43 +10,4 @@ public class CouchsurfingBackendApplication {
         SpringApplication.run(CouchsurfingBackendApplication.class, args);
     }
 
-   /* @Bean
-    CommandLineRunner runner(UserRepository userRepository, TripRepository tripRepository) {
-        return args -> {
-            User user = new User("Test login", "test@gmail.com",
-                    "password123213-ascouib", LocalDateTime.now(), false,
-                    List.of(Authority.ROLE_ADMIN, Authority.ROLE_USER), 9.8, null, null, List.of(), List.of());
-
-            User user1 = new User("Test login3", "test3232@gmail.com",
-                    "password123213-ascouidsb", LocalDateTime.now(), false,
-                    List.of(Authority.ROLE_ADMIN), 9.8, null, null, List.of(), List.of());
-
-            User u1 = userRepository.insert(user);
-            User u2 = userRepository.insert(user1);
-
-            Review review = new Review(ObjectId.get(), u2, true,
-                    ServiceType.SURFER, 10.0, "Cool", LocalDateTime.now());
-
-            Trip trip = new Trip(u1, LocalDateTime.now(), LocalDateTime.now(),
-                    1, "Kharkiv, Ukraine", "Message");
-
-            tripRepository.insert(trip);
-
-            u1.setTrips(List.of(trip));
-            u1.setReviews(List.of(review));
-
-            userRepository.save(u1);
-
-
-            List<User> user = userRepository.findAll();
-
-            user.getFirst().getReviews().forEach(System.out::println);
-
-            List<Trip> trips = tripRepository.findAllByLocation("Kharkiv, Ukraine",
-                    PageRequest.of(0, 1)).getContent();
-
-            trips.forEach(System.out::println);
-
-        };
-    }*/
 }
