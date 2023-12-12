04c0f09cb86261ff499fd63081cfaf1c57e6e0cf
Oleksii Drabchak <drabchak.aleksey@gmail.com>
2023-12-12 14:57:57 +0200

    Added entity no found exception class

---
+++ couchsurfing-backend/src/main/java/com/example/backend/exception/EntityUpdateException.java
@@ -0,0 +1,14 @@
+package com.example.backend.exception;
+
+public class EntityUpdateException extends ApiException {
+    public EntityUpdateException() {
+    }
+
+    public EntityUpdateException(String message) {
+        super(message);
+    }
+
+    public EntityUpdateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
