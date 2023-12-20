78630131d2604fcd6f2324bcfb2d1aa21991eb61
Oleksii Drabchak <drabchak.aleksey@gmail.com>
2023-12-20 22:58:41 +0200

    Added footer component tag to main component   Added routes handling for dinamical components uploading

--- couchsurfing-frontend/src/app/app.module.ts
+++ couchsurfing-frontend/src/app/app.module.ts
@@ -2,12 +2,20 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {RouterOutlet} from "@angular/router";
-import { HeaderComponent } from './components/header/header.component';
+import {RouterModule, RouterOutlet, Routes} from "@angular/router";
+import {HeaderComponent} from './components/header-content/header/header.component';
 import {NgOptimizedImage} from "@angular/common";
-import { SearchComponent } from './components/search/search.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import {SearchComponent} from './components/header-content/search/search.component';
+import {NavbarComponent} from './components/header-content/navbar/navbar.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
 
+
+const routes: Routes = [
+  {path: 'dashboard', component: DashboardComponent},
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: '**', redirectTo: '/dashboard', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
@@ -15,8 +23,11 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     HeaderComponent,
     SearchComponent,
     NavbarComponent,
+    FooterComponent,
+    DashboardComponent,
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     RouterOutlet,
     NgOptimizedImage
