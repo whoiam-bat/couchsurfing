757f31283a2758f145ea8fe344ba6311f97cb96d
Oleksii Drabchak <drabchak.aleksey@gmail.com>
2024-01-10 22:06:53 +0200

    Dashboard tab   Added basic components layout for Dashboard page

--- couchsurfing-frontend/src/app/app.module.ts
+++ couchsurfing-frontend/src/app/app.module.ts
@@ -9,31 +9,41 @@ import {SearchComponent} from './components/header-content/search/search.compone
 import {NavbarComponent} from './components/header-content/navbar/navbar.component';
 import {FooterComponent} from './components/footer/footer.component';
 import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {UserinfoComponent} from './components/dashboard/userinfo/userinfo.component';
+import { CompletionLevelComponent } from './components/dashboard/completion-level/completion-level.component';
+import { ToDoComponent } from './components/dashboard/to-do/to-do.component';
+import { MyTravelsComponent } from './components/dashboard/my-travels/my-travels.component';
+import { LookingHostsComponent } from './components/dashboard/looking-hosts/looking-hosts.component';
 
 
 const routes: Routes = [
-  {path: 'dashboard', component: DashboardComponent},
-  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-  {path: '**', redirectTo: '/dashboard', pathMatch: 'full'}
+    {path: 'dashboard', component: DashboardComponent},
+    {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+    {path: '**', redirectTo: '/dashboard', pathMatch: 'full'}
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    SearchComponent,
-    NavbarComponent,
-    FooterComponent,
-    DashboardComponent,
-  ],
-  imports: [
-    RouterModule.forRoot(routes),
-    BrowserModule,
-    RouterOutlet,
-    NgOptimizedImage
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        SearchComponent,
+        NavbarComponent,
+        FooterComponent,
+        DashboardComponent,
+        UserinfoComponent,
+        CompletionLevelComponent,
+        ToDoComponent,
+        MyTravelsComponent,
+        LookingHostsComponent,
+    ],
+    imports: [
+        RouterModule.forRoot(routes),
+        BrowserModule,
+        RouterOutlet,
+        NgOptimizedImage
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
---
+++ couchsurfing-frontend/src/app/components/dashboard/completion-level/completion-level.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-completion-level',
+  templateUrl: './completion-level.component.html',
+  styleUrls: ['./completion-level.component.css']
+})
+export class CompletionLevelComponent {
+
+}
