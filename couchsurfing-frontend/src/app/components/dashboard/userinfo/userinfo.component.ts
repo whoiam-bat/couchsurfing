0e2350373cd823def330c3dde4b63b30e8265f07
Oleksii Drabchak <drabchak.aleksey@gmail.com>
2024-01-13 23:21:42 +0200

    Frontend   Completed UserInfo component card

--- couchsurfing-frontend/src/app/components/dashboard/userinfo/userinfo.component.ts
+++ couchsurfing-frontend/src/app/components/dashboard/userinfo/userinfo.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-userinfo',
   templateUrl: './userinfo.component.html',
   styleUrls: ['./userinfo.component.css']
 })
-export class UserinfoComponent {
+export class UserinfoComponent implements OnInit {
+
+  selectedOption: string = "Not Accepting Guests";
+  searchOptions: string[] = ["Not Accepting Guests", "Accepting Guests"];
+
+  ngOnInit(): void {
+    this.resetDefaultOption();
+  }
+
+  doSelectOption(option: string): void {
+    this.selectedOption = option;
+  }
+
+  resetDefaultOption(): void {
+    this.selectedOption = "Not Accepting Guests";
+  }
+
+  dropdownList(): void {
+    let list = document.getElementById('accommodation-options')
+    list!.classList.toggle('open')
+  }
 
 }
