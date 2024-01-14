6634b1af7a0e7ed24886e802c818989703e777b6
Oleksii Drabchak <drabchak.aleksey@gmail.com>
2024-01-14 17:14:25 +0200

    Frontend   Completed Account completion level component card

--- couchsurfing-frontend/src/app/components/dashboard/completion-level/completion-level.component.ts
+++ couchsurfing-frontend/src/app/components/dashboard/completion-level/completion-level.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-completion-level',
   templateUrl: './completion-level.component.html',
   styleUrls: ['./completion-level.component.css']
 })
-export class CompletionLevelComponent {
+export class CompletionLevelComponent implements OnInit {
+  progress!: number;
+
+  ngOnInit(): void {
+    this.progress = 55;
+  }
 
 }
