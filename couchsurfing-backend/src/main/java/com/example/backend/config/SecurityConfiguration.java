7c95ca5752cf8f33192b7db92055e01ba40a8102
Oleksii Drabchak <drabchak.aleksey@gmail.com>
2024-01-17 22:59:34 +0200

    Backend   Enabled CORS for client side

--- couchsurfing-backend/src/main/java/com/example/backend/config/SecurityConfiguration.java
+++ couchsurfing-backend/src/main/java/com/example/backend/config/SecurityConfiguration.java
@@ -10,6 +10,12 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -23,19 +29,28 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf(AbstractHttpConfigurer::disable);
-
-        httpSecurity.authorizeHttpRequests(request -> {
-            request.requestMatchers("/api/v1/authentication").permitAll();
-            request.requestMatchers("/api/v1/registration").permitAll();
-            request.anyRequest().permitAll(); // change to anyRequest().authenticated()
-        });
-
-        httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        httpSecurity.authenticationProvider(authenticationProvider)
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(apiConfigurationSource()))
+                .authorizeHttpRequests(request -> {
+                    request.requestMatchers("/api/v1/authentication").permitAll();
+                    request.requestMatchers("/api/v1/registration").permitAll();
+                    request.anyRequest().permitAll(); // change to anyRequest().authenticated()
+                })
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
     }
 
+    CorsConfigurationSource apiConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:4200"
+        ));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "PUT", "DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
