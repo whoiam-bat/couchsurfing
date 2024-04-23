77571a57be94a55dcc569606f21990c775650279
Oleksii Drabchak <drabchak.aleksey@gmail.com>
2024-04-23 21:01:32 +0300

    Added Login component

---
+++ couchsurfing-frontend/src/app/pages/login/login.component.ts
@@ -0,0 +1,52 @@
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthRequest} from 'src/app/services/models';
+import {AuthenticationService} from 'src/app/services/services';
+import {StorageService} from "../../services/storage/storage.service";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+
+  authRequest: AuthRequest = {email: '', password: ''};
+  errorMsg: Array<string> = [];
+
+
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService,
+    private storageService: StorageService
+  ) {
+  }
+
+
+  login() {
+    this.errorMsg = [];
+    this.authService.authenticate({
+      body: this.authRequest
+    }).subscribe({
+      next: (res) => {
+        this.storageService.token = res.accessToken as string;
+        this.storageService.userId = res.userId as string;
+
+        this.router.navigate(['dashboard'])
+      },
+      error: (err) => {
+        console.log(err);
+
+        if (err.error.validationErrors)
+          this.errorMsg = err.error.validationErrors;
+        else
+          this.errorMsg.push(err.error.errorMsg);
+      }
+    })
+  }
+
+  register() {
+    this.router.navigate(['register']);
+  }
+
+}
