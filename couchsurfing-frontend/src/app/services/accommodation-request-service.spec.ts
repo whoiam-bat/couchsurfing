4def1d9552448c8e0c1710e8cbebe18f564c0ccd
Oleksii Drabchak <drabchak.aleksey@gmail.com>
2024-04-23 18:58:31 +0300

    Removed old code

--- couchsurfing-frontend/src/app/services/accommodation-request-service.spec.ts
+++
@@ -1,7 +0,0 @@
-import {AccommodationRequestService} from './accommodation-request-service';
-
-describe('AccommodationRequestService', () => {
-  it('should create an instance', () => {
-    expect(new AccommodationRequestService()).toBeTruthy();
-  });
-});
