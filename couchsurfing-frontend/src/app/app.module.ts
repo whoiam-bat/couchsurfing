2bba1138c97749a4c4e58272464c18890e90ef48
Oleksii Drabchak <drabchak.aleksey@gmail.com>
2024-04-24 17:05:09 +0300

    Updated routes

--- couchsurfing-frontend/src/app/app.module.ts
+++ couchsurfing-frontend/src/app/app.module.ts
@@ -7,13 +7,14 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './pages/login/login.component';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-
+import { RegisterComponent } from './pages/register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
