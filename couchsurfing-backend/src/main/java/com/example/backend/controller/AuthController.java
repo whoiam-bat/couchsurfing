ea682b84bd384936da0511daca94a2aa2577dc88
Oleksii Drabchak <drabchak.aleksey@gmail.com>
2024-04-27 20:30:37 +0300

    Added DTO fields validation

--- couchsurfing-backend/src/main/java/com/example/backend/controller/AuthController.java
+++ couchsurfing-backend/src/main/java/com/example/backend/controller/AuthController.java
@@ -5,13 +5,16 @@ import com.example.backend.model.dto.AuthResponse;
 import com.example.backend.model.dto.RegistrationRequest;
 import com.example.backend.service.AuthService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@Validated
 @Tag(name = "Authentication")
 public class AuthController {
 
@@ -19,12 +22,12 @@ public class AuthController {
 
 
     @PostMapping("/register")
-    public ResponseEntity<AuthResponse> register(@RequestBody RegistrationRequest request) {
+    public ResponseEntity<AuthResponse> register(@RequestBody @Valid RegistrationRequest request) {
         return ResponseEntity.ok(authService.register(request));
     }
 
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthResponse> authenticate(@RequestBody AuthRequest request) {
+    public ResponseEntity<AuthResponse> authenticate(@RequestBody @Valid AuthRequest request) {
         return ResponseEntity.ok(authService.authenticate(request));
     }
 
--- couchsurfing-backend/src/main/java/com/example/backend/model/dto/AuthRequest.java
+++ couchsurfing-backend/src/main/java/com/example/backend/model/dto/AuthRequest.java
@@ -1,5 +1,8 @@
 package com.example.backend.model.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +13,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuthRequest {
+    @Email(message = "Invalid email format.")
     private String email;
+
+    @Pattern(regexp = "^([a-zA-Z]){2,} ([a-zA-Z]){2,}$",
+            message = "Login should represent your full name.")
+    @Size(min = 5, message = "Login length should be at least 5 characters.")
     private String password;
 }
