dbc4d8b82c4d92942624babb58ab5b3c8070fd8f
Oleksii Drabchak <drabchak.aleksey@gmail.com>
2024-05-10 15:48:04 +0300

    Updated Applcation module Updated Application routes

--- couchsurfing-frontend/src/app/app-routing.module.ts
+++ couchsurfing-frontend/src/app/app-routing.module.ts
@@ -2,7 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
-
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { VerifyComponent } from './pages/verify/verify.component';
+import { PublicTripsComponent } from './pages/public-trips/public-trips.component';
+import { CreateTripComponent } from './pages/public-trips/create-trip/create-trip.component';
+import { authGuard } from './services/guard/auth.guard';
+import { EditProfileComponent } from './pages/edit-profile/edit-profile.component';
+import { CouchRequestsComponent } from './pages/couch-requests/couch-requests.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
@@ -13,6 +21,55 @@ const routes: Routes = [
     path: 'register',
     component: RegisterComponent,
   },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'dashboard/public-trips',
+    component: PublicTripsComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'dashboard/public-trips/manage',
+    component: CreateTripComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'dashboard/couch-requests',
+    component: CouchRequestsComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'profile/edit-profile',
+    component: EditProfileComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'profile/:id',
+    component: ProfileComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'verify',
+    component: VerifyComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: '',
+    component: DashboardComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  }
 ];
 
 @NgModule({
--- couchsurfing-frontend/src/app/app.module.ts
+++ couchsurfing-frontend/src/app/app.module.ts
@@ -3,16 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgOptimizedImage, TitleCasePipe } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './pages/login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { RegisterComponent } from './pages/register/register.component';
-import {HeaderComponent} from "./base/header/header.component";
-import {NavbarComponent} from "./base/header/navbar/navbar.component";
-import {SearchComponent} from "./base/header/search/search.component";
-import {FooterComponent} from "./base/footer/footer.component";
-
+import { HeaderComponent } from './components/header/header.component';
+import { AsideBarComponent } from './pages/dashboard/aside-bar/aside-bar.component';
+import { DashboardCollageComponent } from './pages/dashboard/dashboard-collage/dashboard-collage.component';
+import { ToDoListComponent } from './pages/dashboard/to-do-list/to-do-list.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { TravelPlansComponent } from './pages/dashboard/travel-plans/travel-plans.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PublicTripsComponent } from './pages/public-trips/public-trips.component';
+import { NavbarComponent } from './components/header/navbar/navbar.component';
+import { SearchComponent } from './components/header/search/search.component';
+import { CreateTripComponent } from './pages/public-trips/create-trip/create-trip.component';
+import { HttpTokenInterceptor } from './services/interceptor/http-token.interceptor';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ProfileAsideComponent } from './pages/profile/profile-aside/profile-aside.component';
+import { ProfileMainSectionComponent } from './pages/profile/profile-main-section/profile-main-section.component';
+import { EditProfileComponent } from './pages/edit-profile/edit-profile.component';
+import { EditProfileAsideComponent } from './pages/edit-profile/edit-profile-aside/edit-profile-aside.component';
+import { EditProfileMainComponent } from './pages/edit-profile/edit-profile-main/edit-profile-main.component';
+import { AboutFormComponent } from './pages/edit-profile/about-form/about-form.component';
+import { HomeFormComponent } from './pages/edit-profile/home-form/home-form.component';
+import { AboutTabComponent } from './pages/profile/about-tab/about-tab.component';
+import { MyHomeTabComponent } from './pages/profile/my-home-tab/my-home-tab.component';
+import { FeedbackModalComponent } from './pages/feedback-modal/feedback-modal.component';
+import { CouchRequestsComponent } from './pages/couch-requests/couch-requests.component';
+import { ReviewsTabComponent } from './pages/profile/reviews-tab/reviews-tab.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
@@ -20,9 +41,31 @@ import {FooterComponent} from "./base/footer/footer.component";
     LoginComponent,
     RegisterComponent,
     HeaderComponent,
+    ToDoListComponent,
+    DashboardComponent,
+    AsideBarComponent,
+    AsideBarComponent,
+    DashboardCollageComponent,
+    TravelPlansComponent,
+    FooterComponent,
+    PublicTripsComponent,
     NavbarComponent,
     SearchComponent,
-    FooterComponent,
+    CreateTripComponent,
+    ProfileComponent,
+    ProfileAsideComponent,
+    ProfileMainSectionComponent,
+    EditProfileComponent,
+    EditProfileAsideComponent,
+    EditProfileMainComponent,
+    AboutFormComponent,
+    HomeFormComponent,
+    AboutTabComponent,
+    MyHomeTabComponent,
+    FeedbackModalComponent,
+    CouchRequestsComponent,
+    ReviewsTabComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
@@ -33,7 +76,15 @@ import {FooterComponent} from "./base/footer/footer.component";
     TitleCasePipe,
     ReactiveFormsModule,
   ],
-  providers: [HttpClient],
-  bootstrap: [AppComponent]
+  providers: [
+    HttpClient,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpTokenInterceptor,
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
+  exports: [],
 })
 export class AppModule {}
