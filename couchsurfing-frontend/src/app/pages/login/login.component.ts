59a60e64e77dde1959f583eb34c24cd5e2d0a07d
Oleksii Drabchak <drabchak.aleksey@gmail.com>
2024-05-10 11:58:45 +0300

    Implemented login page

--- couchsurfing-frontend/src/app/pages/login/login.component.ts
+++ couchsurfing-frontend/src/app/pages/login/login.component.ts
@@ -1,8 +1,8 @@
-import {Component} from '@angular/core';
-import {Router} from '@angular/router';
-import {AuthRequest} from 'src/app/services/models';
-import {AuthenticationService} from 'src/app/services/services';
-import {StorageService} from "../../services/storage/storage.service";
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthRequest } from 'src/app/services/models';
+import { AuthenticationService } from 'src/app/services/services';
+import { StorageService } from '../../services/storage/storage.service';
 
 @Component({
   selector: 'app-login',
@@ -10,42 +10,38 @@ import {StorageService} from "../../services/storage/storage.service";
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-
-  authRequest: AuthRequest = {email: '', password: ''};
+  authRequest: AuthRequest = { email: '', password: '' };
   errorMsg: Array<string> = [];
-
 
   constructor(
     private router: Router,
     private authService: AuthenticationService,
     private storageService: StorageService
-  ) {
-  }
-
+  ) {}
 
   login() {
     this.errorMsg = [];
-    this.authService.authenticate({
-      body: this.authRequest
-    }).subscribe({
-      next: (res) => {
-        this.storageService.token = res.accessToken as string;
+    this.authService
+      .authenticate({
+        body: this.authRequest,
+      })
+      .subscribe({
+        next: (res) => {
+          this.storageService.token = res.accessToken as string;
+          this.storageService.email = this.authRequest.email;
+          this.router.navigate(['dashboard']);
+        },
+        error: (err) => {
+          console.log(err);
 
-        this.router.navigate(['dashboard'])
-      },
-      error: (err) => {
-        console.log(err);
-
-        if (err.error.validationErrors)
-          this.errorMsg = err.error.validationErrors;
-        else
-          this.errorMsg.push(err.error.errorMsg);
-      }
-    })
+          if (err.error.validationErrors)
+            this.errorMsg = err.error.validationErrors;
+          else this.errorMsg.push(err.error.error);
+        },
+      });
   }
 
   register() {
     this.router.navigate(['register']);
   }
-
 }
